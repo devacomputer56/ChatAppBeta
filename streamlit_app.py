effeec5bbbd1be7a2c2750bb9f2d51595ce71b36
devacomputer56 <136178711+devacomputer56@users.noreply.github.com>
2024-08-23 21:10:51 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ elif 10<= jptime <17:
 elif 17<= jptime or jptime<5 :
     st.title(f"こんばんは　{name}")
     model = genai.GenerativeModel('gemini-1.5-flash')
-    fin = model.generate_content("1日の終わりの1日を労う文を3文で考えて")
+    fin = model.generate_content("1日の終わりの1日を労う文を敬語かつ3文で考えて")
     fin_book = fin.text
     st.write(fin_book)
 else :
