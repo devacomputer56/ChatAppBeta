64e6511db6d6b60c7e062dc3f506f11a1905bb0c
devacomputer56 <136178711+devacomputer56@users.noreply.github.com>
2024-08-25 00:12:42 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,6 +45,11 @@ with st.sidebar:
         zatu = model.generate_content("面白い雑学を５文以内でわかりやすく教えて")
         zatu_book = zatu.text
         st.write(zatu_book)
+    if st.button("会話のテーマを得る"):
+        model = genai.GenerativeModel('gemini-1.5-flash')
+        insp = model.generate_content("会話の話題を3文で考えて")
+        insp_book = insp.text
+        st.write(insp_book)
 
     st.title("Command")
 now = datetime.datetime.now()
