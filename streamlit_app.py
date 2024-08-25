d70c383f61313388960dfd4a1c1ce5ca1e852cfc
devacomputer56 <136178711+devacomputer56@users.noreply.github.com>
2024-08-25 14:21:25 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -162,10 +162,10 @@ if prompt := st.chat_input("ご用件を教えてください"):
         st.write(assistant_response)
 
 with st.sidebar:
-    if st.button("再生成を依頼する") :
+    if st.button("再生成する") :
         add = st.text_input("さらに尋ねてみてください。")
         model = genai.GenerativeModel('gemini-1.5-flash')
-        fut = model.generate_content(prompt")
+        fut = model.generate_content(f"{prompt}という質問をもう一度考えてください"))
         fut_book = fut.text
         st.write(fut_book)
     
