ba59e9f64201cbf63fc8d5f13c9744c0e08f90d4
devacomputer56 <136178711+devacomputer56@users.noreply.github.com>
2024-08-25 17:19:23 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -149,7 +149,7 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt = st.chat_input("ご用件を教えてください"):
+if prompt := st.chat_input("ご用件を教えてください"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
